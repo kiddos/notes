5e54e454c1514f30db44896cd890780650088943
kiddos <kiddo831007@gmail.com>
2024-11-10 18:43:21 +0800

    libcpr sample

---
+++ cpp/cpr/sample/sample.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <cpr/cpr.h>
+
+int main(void) {
+  using namespace cpr;
+
+  cpr::Response r = cpr::Get(cpr::Url{"https://www.google.com/"});
+  std::cout << r.status_code << std::endl;
+  std::cout << r.header["content-type"] << std::endl;
+  std::cout << r.text << std::endl;
+  return 0;
+}
