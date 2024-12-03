77966b6024153894524f822c66253a8904a3f255
kiddos <kiddo831007@gmail.com>
2024-12-03 10:56:19 +0800

    curl sample

---
+++ cpp/curl/sample/sample.cc
@@ -0,0 +1,14 @@
+#include "request.h"
+#include <iostream>
+
+int main(void) {
+  Request request;
+  std::string response = request.get("https://jsonplaceholder.typicode.com/posts/1");
+  std::cout << response << std::endl;
+
+  std::string data = R"({"title": "foo", "body": "bar", "userId": 1})";
+  std::unordered_map<std::string, std::string> headers = {{"Content-Type", "application/json"}};
+  response = request.post("https://jsonplaceholder.typicode.com/posts", data, headers);
+  std::cout << response << std::endl;
+  return 0;
+}
