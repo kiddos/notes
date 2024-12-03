e80c49743ac7346d4707094f3950bd2ae63a6911
kiddos <kiddo831007@gmail.com>
2024-12-03 11:24:11 +0800

    eigen sample

---
+++ cpp/eigen/sample/sample.cc
@@ -0,0 +1,17 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <chrono>
+
+int main(void) {
+  constexpr int SIZE = 1024;
+  Eigen::MatrixXd A = Eigen::MatrixXd::Random(SIZE, SIZE);
+  Eigen::MatrixXd B = Eigen::MatrixXd::Random(SIZE, SIZE);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  Eigen::MatrixXd C = A * B;
+  auto end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double> elapsed = end - start;
+  std::cout << "Time taken for multiplication: " << elapsed.count() << " seconds" << std::endl;
+  return 0;
+}
