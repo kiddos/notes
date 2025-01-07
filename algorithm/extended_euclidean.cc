2b7f46213835fafbeab21319c7b1c60f916ce069
kiddos <kiddo831007@gmail.com>
2025-01-07 10:58:18 +0800

    extended euclidean

--- algorithm/extended_euclidean.cc
+++ algorithm/extended_euclidean.cc
@@ -4,6 +4,15 @@ using namespace std;
 
 // euclidean algorithm only finds gcd(a, b)
 // extended_euclidean finds a * x + b * y = gcd(a, b)
+// a * x + b * y = g
+// => a * x1 + b * y1 = g
+// => b * x2 + (a % b) * y2 = g
+// => a * x1 + b * y1 = b * x2 + (a - a / b * b) * y2
+// => a * x1 + b * y1 = a * y2 + (b * x2 - a / b * b * y2) 
+// => a * x1 + b * y1 = a * y2 + b * (x2 - a / b * y2);
+//
+// => x1 = y2
+// => y1 = x2 - a / b * y2;
 
 int extended_euclidean(int a, int b, int& x, int& y) {
   if (b == 0) {
