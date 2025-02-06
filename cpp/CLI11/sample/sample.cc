af9f09476451b51119b930e58b6d5ae26bc8f56f
kiddos <kiddo831007@gmail.com>
2025-02-06 11:34:05 +0800

    CLI11 sample

---
+++ cpp/CLI11/sample/sample.cc
@@ -0,0 +1,15 @@
+#include <string>
+
+#include "CLI/CLI.hpp"
+
+int main(int argc, char* argv[]) {
+  CLI::App app{"App description"};
+  argv = app.ensure_utf8(argv);
+
+  std::string filename = "default";
+  app.add_option("-f,--file", filename, "A help string");
+  CLI11_PARSE(app, argc, argv);
+
+  std::cout << "filename=" << filename << std::endl;
+  return 0;
+}
