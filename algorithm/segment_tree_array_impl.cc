3662706e187b2c0ce23055d46a53763fbb828f6a
kiddos <kiddo831007@gmail.com>
2025-02-16 19:31:55 +0800

    update segment tree array impl

--- algorithm/segment_tree_array_impl.cc
+++ algorithm/segment_tree_array_impl.cc
@@ -2,57 +2,72 @@
 
 using namespace std;
 
-class SegmentTree {
+template <typename T, typename F>
+class SegmentTreeImpl {
  public:
-  SegmentTree(vector<int>& nums) {
-    int n = nums.size();
+  SegmentTreeImpl(int n) : n(n) {
     int x = (int)(ceil(log2(n)));
     int max_size = 2 * (int)pow(2, x) - 1;
-    sums_ = vector<int>(max_size);
-    function<void(int, int, int)> build_tree = [&](int i, int tl, int tr) {
-      if (tl == tr) {
-        sums_[i] = nums[tl];
-        return;
-      }
-      int tm = tl + (tr - tl) / 2;
-      build_tree(i * 2 + 1, tl, tm);
-      build_tree(i * 2 + 2, tm + 1, tr);
-
-      update(i);
-    };
-
-    build_tree(0, 0, n - 1);
+    data_ = vector<T>(max_size);
   }
 
-  int query(int i, int tl, int tr, int ql, int qr) {
-    if (ql > qr) return 0;
-    if (tr < ql || tl > qr) return 0;
-    if (tl >= ql && tr <= qr) return sums_[i];
+  SegmentTreeImpl(vector<T>& data) {
+    int n = data.size();
+    int x = (int)(ceil(log2(n)));
+    int max_size = 2 * (int)pow(2, x) - 1;
+    data_ = vector<T>(max_size);
+    build_tree(data, 0, 0, n-1);
+  }
+
+  void build_tree(vector<T>& data, int i, int tl, int tr) {
+    if (tl == tr) {
+      data_[i] = data[tl];
+      return;
+    }
+    int tm = tl + (tr - tl) / 2;
+    build_tree(data, i * 2 + 1, tl, tm);
+    build_tree(data, i * 2 + 2, tm + 1, tr);
+    data_[i] = merge(data_[i * 2 + 1], data_[i * 2 + 2]);
+  }
+
+  T query(int i, int tl, int tr, int ql, int qr) {
+    if (ql > qr) return T();
+    if (tr < ql || tl > qr) return T();
+    if (tl >= ql && tr <= qr) return data_[i];
 
     int tm = tl + (tr - tl) / 2;
-    return query(i * 2 + 1, tl, tm, ql, min(tm, qr)) +
-           query(i * 2 + 2, tm + 1, tr, max(tm, ql), qr);
+    T left = query(i * 2 + 1, tl, tm, ql, min(tm, qr));
+    T right = query(i * 2 + 2, tm + 1, tr, max(tm, ql), qr);
+    return merge(left, right);
   }
 
-  void update(int i, int tl, int tr, int index, int val) {
+  T query(int ql, int qr) { return query(0, 0, n - 1, ql, qr); }
+
+  void update(int i, int tl, int tr, int index, const T& val) {
     if (tl == tr) {
-      sums_[i] = val;
+      data_[i] = val;
       return;
     }
 
     int tm = tl + (tr - tl) / 2;
-    if (index <= tm)
+    if (index <= tm) {
       update(i * 2 + 1, tl, tm, index, val);
-    else
+    } else {
       update(i * 2 + 2, tm + 1, tr, index, val);
-    update(i);
+    }
+    data_[i] = merge(data_[i * 2 + 1], data_[i * 2 + 2]);
   }
 
- private:
-  vector<int> sums_;
+  void update(int index, T val) { update(0, 0, n - 1, index, val); }
 
-  inline void update(int i) { sums_[i] = sums_[i * 2 + 1] + sums_[i * 2 + 2]; }
+ private:
+  int n;
+  vector<T> data_;
+
+  inline T merge(T x, T y) { return F{}(x, y); }
 };
+
+using SegmentTree = SegmentTreeImpl<int, std::plus<int>>;
 
 int main(void) {
   ios::sync_with_stdio(false);
