bd23955eca83bdefb44eb9dad7736deb5c245430
kiddos <kiddo831007@gmail.com>
2025-06-04 09:28:29 +0800

    update suffix array

--- algorithm/suffix_array.cc
+++ algorithm/suffix_array.cc
@@ -4,67 +4,53 @@ using namespace std;
 
 #define ALPHABET 256
 
-vector<int> sort_cyclic_shift(const string& s) {
+vector<int> suffix_array(const string& t) {
+  const string s = t + "$";
+  vector<pair<pair<int,int>, int>> suff;
   int n = s.length();
-  vector<int> p(n), c(n), chars(ALPHABET);
-  for (char ch : s) {
-    chars[ch]++;
+  for (int i = 0; i < n; ++i) {
+    suff.push_back({{s[i], s[i]}, i});
   }
-  for (int i = 1; i < ALPHABET; ++i) {
-    chars[i] += chars[i-1];
-  }
-  for (int i = n-1; i >= 0; --i) {
-    p[--chars[s[i]]] = i;
-  }
+  sort(suff.begin(), suff.end());
 
-  c[p[0]] = 0;
-  int classes = 1;
-  for (int i = 1; i < n; ++i) {
-    if (s[p[i]] != s[p[i-1]]) {
-      classes++;
-    }
-    c[p[i]] = classes-1;
-  }
+  vector<int> equiv(n);
 
-  vector<int> pn(n), cn(n), chars_n(n);
-  for (int k = 0; (1<<k) < n; ++k) {
-    for (int i = 0; i < n; ++i) {
-      pn[i] = (p[i] - (1<<k) + n) % n;
-    }
-
-    fill(chars_n.begin(), chars_n.end(), 0);
-    for (int i = 0; i < n; ++i) {
-      chars_n[c[pn[i]]]++;
-    }
+  auto compute_equiv = [&]() {
     for (int i = 1; i < n; ++i) {
-      chars_n[i] += chars_n[i-1];
+      auto [p1, i1] = suff[i-1];
+      auto [p2, i2] = suff[i];
+      equiv[i2] = equiv[i1] + (p2 > p1);
     }
-    for (int i = n-1; i >= 0; --i) {
-      p[--chars_n[c[pn[i]]]] = pn[i];
+  };
+
+  compute_equiv();
+
+  for (int len = 1; len < n; len *= 2) {
+    for (auto& [p, idx]: suff) {
+      p.first = equiv[idx];
+      p.second = equiv[(idx + len) % n];
     }
-    cn[p[0]] = 0;
-    classes = 1;
-    for (int i = 1; i < n; ++i) {
-      pair<int,int> current = {c[p[i]], c[(p[i] + (1<<k)) % n]};
-      pair<int,int> pre = {c[p[i-1]], c[(p[i-1] + (1<<k)) % n]};
-      if (current != pre) {
-        classes++;
-      }
-      cn[p[i]] = classes-1;
-    }
-    c.swap(cn);
+    sort(suff.begin(), suff.end());
+
+    compute_equiv();
   }
-  return p;
+  vector<int> sa(n);
+  for (int i = 0; i < n; ++i) {
+    sa[i] = suff[i].second;
+  }
+  return sa;
 }
 
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  string s = "aabbaacc$";
+  string s = "abcbcba";
   // string s = "aaba";
-  vector<int> p = sort_cyclic_shift(s);
-  for (int idx : p) cout << idx << " ";
+  vector<int> p = suffix_array(s);
+  for (int idx : p) {
+    cout << idx << " ";
+  }
   cout << endl;
   return 0;
 }
