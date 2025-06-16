904ae455cd1390cfc5603c2fc647e5409e3e96b9
kiddos <kiddo831007@gmail.com>
2025-06-16 10:56:21 +0800

    trie implementation

---
+++ algorithm/trie.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+
+template <int NODE_SIZE>
+struct TrieNodeImpl {
+  TrieNodeImpl* children[NODE_SIZE];
+  int count;
+
+  TrieNodeImpl() : children{nullptr}, count(0) {}
+  ~TrieNodeImpl() {
+    for (int i = 0; i < NODE_SIZE; ++i) {
+      delete children[i];
+    }
+  }
+};
+
+using TrieNode = TrieNodeImpl<26>;
+
+void insert(TrieNode* root, const string& s) {
+  TrieNode* it = root;
+  root->count++;
+  for (char ch : s) {
+    int c = ch - 'a';
+    if (!it->children[c]) {
+      it->children[c] = new TrieNode();
+    }
+    it = it->children[c];
+    it->count++;
+  }
+}
+
+TrieNode* remove_node(TrieNode* node, const string& s, int i) {
+  if (!node) {
+    return nullptr;
+  }
+
+  if (--node->count == 0) {
+    delete node;
+    return nullptr;
+  }
+
+  if (i == (int)s.length()) {
+    delete node;
+    return nullptr;
+  }
+
+  int c = s[i] - 'a';
+  node->children[c] = remove_node(node->children[c], s, i+1);
+  return node;
+}
+
+void remove(TrieNode* root, const string& s) {
+  remove_node(root, s, 0);
+}
+
+bool query(TrieNode* root, const string& prefix) {
+  TrieNode* it = root;
+  for (char ch : prefix) {
+    int c = ch-'a';
+    if (!it->children[c]) {
+      return false;
+    }
+    it = it->children[c];
+  }
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  vector<string> strs = {"apple", "banana", "watermelon", "awesome"};
+  unique_ptr<TrieNode> root(new TrieNode());
+  for (string& s : strs) {
+    insert(root.get(), s);
+  }
+
+  assert(query(root.get(), "app"));
+  assert(query(root.get(), "ba"));
+  assert(query(root.get(), "water"));
+  assert(query(root.get(), "awe"));
+
+  remove(root.get(), "apple");
+
+  assert(!query(root.get(), "app"));
+  assert(query(root.get(), "a"));
+  return 0;
+}
