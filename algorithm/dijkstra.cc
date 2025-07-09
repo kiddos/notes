2f75da76d23ee929957bfc5be2c699999c5d92ac
kiddos <kiddo831007@gmail.com>
2025-07-09 15:25:00 +0800

    dijkstra

--- algorithm/dijkstra.cc
+++ algorithm/dijkstra.cc
@@ -2,61 +2,57 @@
 
 using namespace std;
 
-class Graph {
- public:
-  Graph(int n) : adj_(n) {}
+constexpr int inf = numeric_limits<int>::max();
 
-  void add_edge(int u, int v, int cost) {
-    adj_[u].push_back(make_pair(v, cost));
-  }
-
-  vector<int> dijkstra(int from) {
-    int n = adj_.size();
-    priority_queue<pair<int, int>> pq;
-    vector<int> dist(n, numeric_limits<int>::max());
-    dist[from] = 0;
-    pq.push({from, 0});
-    while (!pq.empty()) {
-      auto node = pq.top();
-      pq.pop();
-      for (auto neighbor : adj_[node.first]) {
-        int new_dist = node.second + neighbor.second;
-        if (new_dist < dist[neighbor.first]) {
-          dist[neighbor.first] = new_dist;
-          pq.push({neighbor.first, new_dist});
-        }
+vector<int> dijkstra(vector<vector<pair<int,int>>> adj, int from) {
+  int n = adj.size();
+  priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
+  vector<int> dists(n, inf);
+  dists[from] = 0;
+  pq.push({0, from});
+  while (!pq.empty()) {
+    auto [dist, node]  = pq.top();
+    pq.pop();
+    for (auto [next_node, edge_weight] : adj[node]) {
+      int new_dist = dist + edge_weight;
+      if (new_dist < dists[next_node]) {
+        dists[next_node] = new_dist;
+        pq.push({new_dist, next_node});
       }
     }
-    return dist;
   }
-
- private:
-  vector<vector<pair<int, int>>> adj_;
-};
+  return dists;
+}
 
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
   const int n = 6;
-  Graph graph(n);
-  graph.add_edge(0, 1, 5);
-  graph.add_edge(0, 2, 1);
-  graph.add_edge(1, 3, 3);
-  graph.add_edge(1, 4, 20);
-  graph.add_edge(1, 2, 2);
-  graph.add_edge(2, 1, 3);
-  graph.add_edge(2, 4, 12);
-  graph.add_edge(3, 2, 3);
-  graph.add_edge(3, 4, 2);
-  graph.add_edge(3, 5, 6);
-  graph.add_edge(4, 5, 1);
+  vector<array<int,3>> edges;
+  edges.push_back({0, 1, 5});
+  edges.push_back({0, 2, 1});
+  edges.push_back({1, 3, 3});
+  edges.push_back({1, 4, 20});
+  edges.push_back({1, 2, 2});
+  edges.push_back({2, 1, 3});
+  edges.push_back({2, 4, 12});
+  edges.push_back({3, 2, 3});
+  edges.push_back({3, 4, 2});
+  edges.push_back({3, 5, 6});
+  edges.push_back({4, 5, 1});
+
+  vector<vector<pair<int,int>>> adj(n);
+  for (auto [u, v, cost] : edges) {
+    adj[u].push_back({v, cost});
+    adj[v].push_back({u, cost});
+  }
 
   for (int from = 0; from < n; ++from) {
-    vector<int> dist = graph.dijkstra(from);
-    for (int to = 0; to < dist.size(); ++to) {
+    vector<int> dist = dijkstra(adj, from);
+    for (int to = 0; to < n; ++to) {
       cout << "distance from " << from << " to " << to << " = ";
-      if (dist[to] == numeric_limits<int>::max()) cout << "INFINITE";
+      if (dist[to] == inf) cout << "INFINITE";
       else cout << dist[to];
       cout << endl;
     }
