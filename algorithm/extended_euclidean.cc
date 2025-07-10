230435300247c1f2c57e39c6392986d7fb809686
kiddos <kiddo831007@gmail.com>
2025-07-10 08:57:08 +0800

    extended euclidean

--- algorithm/extended_euclidean.cc
+++ algorithm/extended_euclidean.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = long long;
 
 // euclidean algorithm only finds gcd(a, b)
 // extended_euclidean finds a * x + b * y = gcd(a, b)
@@ -14,15 +15,15 @@ using namespace std;
 // => x1 = y2
 // => y1 = x2 - a / b * y2;
 
-int extended_euclidean(int a, int b, int& x, int& y) {
+i64 extended_euclidean(i64 a, i64 b, i64& x, i64& y) {
   if (b == 0) {
     x = 1;
     y = 0;
     return a;
   }
 
-  int x1 = 0, y1 = 0;
-  int d = extended_euclidean(b, a % b, x1, y1);
+  i64 x1 = 0, y1 = 0;
+  i64 d = extended_euclidean(b, a % b, x1, y1);
   x = y1;
   y = x1 - y1 * (a / b);
   return d;
@@ -32,9 +33,9 @@ int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int a = 10, b = 15;
-  int x = 0, y = 0;
-  int d = extended_euclidean(a, b, x, y);
+  i64 a = 10, b = 15;
+  i64 x = 0, y = 0;
+  i64 d = extended_euclidean(a, b, x, y);
   assert(d == 5);
   assert(a * x + b * y == d);
   return 0;
