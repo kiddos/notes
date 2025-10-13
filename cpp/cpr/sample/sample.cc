69604279710ee0aea0cd38d0c0ab7c9a9dfed367
kiddos <kiddo831007@gmail.com>
2025-10-13 11:18:16 +0800

    update cpr sample

--- cpp/cpr/sample/sample.cc
+++ cpp/cpr/sample/sample.cc
@@ -3,11 +3,16 @@
 #include <iostream>
 
 int main(void) {
-  using namespace cpr;
-
   cpr::Response r = cpr::Get(cpr::Url{"https://www.google.com/"});
   std::cout << r.status_code << std::endl;
   std::cout << r.header["content-type"] << std::endl;
+  std::cout << r.text << std::endl;
+
+  r = cpr::Post(cpr::Url{"http://www.httpbin.org/post"},
+                cpr::Payload{{"key1", "value1"}, {"key2", "value2"}});
+
+  std::cout << "Status Code: " << r.status_code << std::endl;
+  std::cout << "Response Text (Form Data):" << std::endl;
   std::cout << r.text << std::endl;
   return 0;
 }
