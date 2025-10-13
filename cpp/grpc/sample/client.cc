d1c271c34873fe2d3acb09231012e4a58031455d
kiddos <kiddo831007@gmail.com>
2025-10-13 12:12:15 +0800

    grpc sample

---
+++ cpp/grpc/sample/client.cc
@@ -0,0 +1,28 @@
+#include <grpcpp/grpcpp.h>
+
+#include "sample.grpc.pb.h"
+
+using grpc::ClientContext;
+using grpc::Status;
+
+int main(void) {
+  auto channel =
+      grpc::CreateChannel("localhost:50050", grpc::InsecureChannelCredentials());
+  std::unique_ptr<Greeter::Stub> stub = Greeter::NewStub(channel);
+
+  HelloRequest request;
+  request.set_name("world");
+
+  HelloReply reply;
+  ClientContext context;
+  Status status = stub->SayHello(&context, request, &reply);
+
+  if (status.ok()) {
+    std::cout << reply.message() << std::endl;
+  } else {
+    std::cout << status.error_code() << ": " << status.error_message()
+              << std::endl;
+  }
+
+  return 0;
+}
---
+++ cpp/grpc/sample/server.cc
@@ -0,0 +1,38 @@
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <absl/strings/str_format.h>
+
+#include "sample.pb.h"
+#include "sample.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+class GreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext* context, const HelloRequest* request,
+                  HelloReply* reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+};
+
+int main(void) {
+  int port = 50050;
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  GreeterServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  server->Wait();
+  return 0;
+}
