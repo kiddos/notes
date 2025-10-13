525beb7dea8790b22cb6cb2f8d4a9f4db3e35fd6
kiddos <kiddo831007@gmail.com>
2025-10-13 09:08:05 +0800

    update jsoncpp sample

--- cpp/jsoncpp/sample/sample.cc
+++ cpp/jsoncpp/sample/sample.cc
@@ -1,14 +1,28 @@
-#include <iostream>
 #include <json/json.h>
+
+#include <iostream>
 
 int main() {
   Json::Value root;
   root["name"] = "John Doe";
   root["age"] = 30;
 
-  Json::StreamWriterBuilder builder;
-  builder["indentation"] = "  ";
-  const std::string json = Json::writeString(builder, root);
+  // json to string
+  Json::StreamWriterBuilder writer_builder;
+  writer_builder["indentation"] = "  ";
+  std::string json = Json::writeString(writer_builder, root);
   std::cout << json << std::endl;
+
+  // string to json
+  JSONCPP_STRING err;
+  Json::Value value;
+  Json::CharReaderBuilder reader_builder;
+  const std::unique_ptr<Json::CharReader> reader(
+      reader_builder.newCharReader());
+  if (!reader->parse(json.c_str(), json.c_str() + json.length(), &value,
+                     &err)) {
+    std::cout << "error: " << err << std::endl;
+  }
+
   return 0;
 }
