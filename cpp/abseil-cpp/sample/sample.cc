87c84b85a9db31bc252e33a14fa31dd3b48886e3
kiddos <kiddo831007@gmail.com>
2025-10-14 08:43:11 +0800

    update abseil-cpp sample

--- cpp/abseil-cpp/sample/sample.cc
+++ cpp/abseil-cpp/sample/sample.cc
@@ -2,11 +2,15 @@
 #include <string>
 #include <vector>
 
-#include "absl/strings/str_join.h"
+#include <absl/strings/str_join.h>
+#include <absl/container/flat_hash_map.h>
 
 int main(void) {
   std::vector<std::string> words = {"This", "is", "a", "sentence"};
   std::string sentence = absl::StrJoin(words, " ");
   std::cout << sentence << std::endl;
+
+  absl::flat_hash_map<std::string, std::string> data;
+  data["key"] = "value";
   return 0;
 }
