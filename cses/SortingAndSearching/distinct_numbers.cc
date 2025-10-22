49078e7b3b160171afbc579d7d8bc71ad1e83067
kiddos <kiddo831007@gmail.com>
2025-10-22 13:52:32 +0800

    cses

---
+++ cses/SortingAndSearching/distinct_numbers.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+
+void solve() {
+  int n = 0;
+  cin >> n;
+  vector<int> x(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> x[i];
+  }
+  set<int> s(x.begin(), x.end());
+  int ans = s.size();
+  cout << ans << endl;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+  return 0;
+}
