66e73940cb8a947cf13201e0fb3c0bdf4eed5c07
kiddos <kiddo831007@gmail.com>
2026-01-28 09:49:05 +0800

    update glfw example

--- cpp/glfw/example/example.cc
+++ cpp/glfw/example/example.cc
@@ -15,8 +15,8 @@ int main() {
     return -1;
   }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle", nullptr, nullptr);
