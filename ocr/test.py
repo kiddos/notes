a5f91af398c7db4eb1be68d8e46e808db0acc416
kiddos <kiddo831007@gmail.com>
2026-03-05 11:35:07 +0800

    ocr notes

---
+++ ocr/test.py
@@ -0,0 +1,16 @@
+from paddleocr import PaddleOCR
+
+
+ocr = PaddleOCR(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False)
+
+result = ocr.predict(
+    input="general_ocr_002.png")
+
+# Visualize the results and save the JSON results
+for res in result:
+    res.print()
+    res.save_to_img("output")
+    res.save_to_json("output")
