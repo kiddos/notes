33a6aa428cb54e5d700c7950fd4b0ff647f2e5eb
kiddos <kiddo831007@gmail.com>
2026-04-21 02:07:01 +0800

    update gtest bazel example

--- cpp/gtest/bazel-example/test1.cc
+++ cpp/gtest/bazel-example/test1.cc
@@ -4,9 +4,3 @@
 TEST(SampleTest, Addition) {
     EXPECT_EQ(2 + 2, 4);
 }
-
-// Main function for running tests
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
